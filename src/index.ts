8572c1ba89038dc1227278335a26ca8e5defeb2d
Taejun Park <thecats1105@gmail.com>
2025-05-08 20:42:00 +0900

    feat: improve functions

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,32 @@ import 'dotenv/config'
 import axios from 'axios'
 import { Client } from 'discord.js-selfbot-v13'
 import Discord from 'discord.js-selfbot-v13'
-// import moment from 'moment'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
 const { CONFIG_URL, TOKEN } = process.env
 
 interface Config {
   APPLICATION_ID: string
-  type: string
+  type?: 'PLAYING' | 'STREAMING' | 'LISTENING' | 'WATCHING' | 'COMPETING'
   name?: string
   details?: string
-  startTimeStamp?: string
-  endTimeStamp?: string
+  state?: string
+  streamURL?: string
+  party?: {
+    size: {
+      current: number
+      max: number
+    }
+  }
+  setLocalTime?: boolean
+  timezone?: string
+  startTimestamp?: number
+  endTimestamp?: number
   assets?: {
     large_image?: string
     large_text?: string
@@ -22,6 +38,7 @@ interface Config {
     label: string
     url: string
   }[]
+  refreshInterval?: number
 }
 
 let config: Config
@@ -41,27 +58,51 @@ if (!CONFIG_URL) {
   }
 }
 
+function getAMInTimezone(timezone: string): number {
+  const now = dayjs().tz(timezone)
+  const AM = now.startOf('day')
+  return AM.valueOf()
+}
+
 const client = new Client()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user?.username}`)
 })
 
-const startTimestamps = {
-  startTime: Date.now()
-}
-
 setInterval(() => {
   const RPC = new Discord.RichPresence(client)
     .setApplicationId(config.APPLICATION_ID)
-    .setType(config.type as 'PLAYING' | 'LISTENING' | 'WATCHING' | 'COMPETING')
-    .setStartTimestamp(startTimestamps.startTime)
-
+    .setParty({ max: 10, current: 1 })
+  if (config.type) {
+    RPC.setType(config.type)
+    if (config.type === 'STREAMING' && config.streamURL) {
+      RPC.setURL(config.streamURL)
+    }
+  }
   if (config.name) {
     RPC.setName(config.name)
   }
   if (config.details) {
     RPC.setDetails(config.details)
+  }
+  if (config.state) {
+    RPC.setState(config.state)
+  }
+  if (config.party) {
+    RPC.setParty({
+      max: config.party.size.max,
+      current: config.party.size.current
+    })
+  }
+  if (config.setLocalTime && config.timezone) {
+    RPC.setStartTimestamp(getAMInTimezone(config.timezone))
+  }
+  if (config.startTimestamp) {
+    RPC.setStartTimestamp(config.startTimestamp)
+  }
+  if (config.endTimestamp) {
+    RPC.setEndTimestamp(config.endTimestamp)
   }
   if (config.assets) {
     if (config.assets.large_image) {
@@ -84,7 +125,7 @@ setInterval(() => {
   }
 
   client.user?.setActivity(RPC)
-}, 10000)
+}, config.refreshInterval || 15000)
 
 try {
   await client.login(TOKEN)
