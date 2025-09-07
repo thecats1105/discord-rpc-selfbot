a6e8f8888c5a1fba93176416135d87104a60a336
Taejun Park <thecats1105@gmail.com>
2025-09-07 23:54:40 +0900

    refactor(types): use zod for config type checking

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,29 +1,44 @@
-export interface Config {
-  APPLICATION_ID: string
-  type?: 'PLAYING' | 'STREAMING' | 'LISTENING' | 'WATCHING' | 'COMPETING'
-  name?: string
-  details?: string
-  state?: string
-  streamURL?: string
-  party?: {
-    size: {
-      current: number
-      max: number
-    }
-  }
-  setLocalTime?: boolean
-  timezone?: string
-  startTimestamp?: number
-  endTimestamp?: number
-  assets?: {
-    large_image?: string
-    large_text?: string
-    small_image?: string
-    small_text?: string
-  }
-  buttons?: {
-    label: string
-    url: string
-  }[]
-  refreshInterval?: number
-}
+import * as z from 'zod'
+
+const Config = z.object({
+  APPLICATION_ID: z.string(),
+  type: z
+    .enum(['PLAYING', 'STREAMING', 'LISTENING', 'WATCHING', 'COMPETING'])
+    .optional(),
+  name: z.string().optional(),
+  details: z.string().optional(),
+  state: z.string().optional(),
+  streamURL: z.string().url().optional(),
+  party: z
+    .object({
+      size: z.object({
+        current: z.number(),
+        max: z.number()
+      })
+    })
+    .optional(),
+  setLocalTime: z.boolean().optional(),
+  timezone: z.string().optional(),
+  startTimestamp: z.number().optional(),
+  endTimestamp: z.number().optional(),
+  assets: z
+    .object({
+      large_image: z.string().optional(),
+      large_text: z.string().optional(),
+      small_image: z.string().optional(),
+      small_text: z.string().optional()
+    })
+    .optional(),
+  buttons: z
+    .array(
+      z.object({
+        label: z.string(),
+        url: z.string().url()
+      })
+    )
+    .max(2)
+    .optional(),
+  refreshInterval: z.number().min(15000).max(240000).optional() // Minimum 15000 ms (15 seconds) Maximum 240000 ms (290 seconds)
+})
+
+export type Config = z.infer<typeof Config>
